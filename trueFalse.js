c3ed24fcc8aa7263725b29ac3b6e8f0783969903
Sajjadul <sbappyi200@gmail.com>
2021-02-02 17:02:36 +0600

    true or false check

---
+++ trueFalse.js
@@ -0,0 +1,21 @@
+//false value
+// ''
+// null
+// NaN
+// 0
+
+// true value
+// ' '
+// 'null'
+// []
+// {}
+// 1
+
+
+const input = 1;
+if(input){
+    console.log('True value');
+}
+else{
+    console.log('False value');
+}
